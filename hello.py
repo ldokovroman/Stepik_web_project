3a8e49265aa220a2800e39767fa24cb53030cfb1
ldokovroman <8922684071@mail.ru>
2023-03-04 00:18:20 +0300

    fixed all

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
+bind = "0.0.0.0:8080"
 def wsgi_app(environ, start_response):
     status = "200 OK"
     headers = [("Content-Type", "text/plain")]
-    body = "\r\n".join(environ["QUERY_STRING"].split('&')).encode()
+    body = environ["QUERY_STRING"].replace('&', "\r\n").encode()
     start_response(status, headers)
     return [body]
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c hello.py hello:wsgi_app

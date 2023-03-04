de978cfbbd11bbc96686e0b2ae1684497ede97df
ldokovroman <8922684071@mail.ru>
2023-03-05 01:09:28 +0300

    first adding django app

---
+++ ask/ask/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("qa.urls")),
+    path("login/", include("qa.urls")),
+    path("signup/", include("qa.urls")),
+    path("question/<int:id>/", include("qa.urls")),
+    path("ask/", include("qa.urls")),
+    path("popular/", include("qa.urls")),
+    path("popular/", include("qa.urls")),
+    path("new/", include("qa.urls"))
+]
--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c hello.py hello:wsgi_app
+sudo gunicorn -c etc/ask_conf.py ask.wsgi:application

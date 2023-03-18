a65ee365a005d972b1c43addf32d954ea3f9f07d
ldokovroman <8922684071@mail.ru>
2023-03-18 17:42:29 +0300

    added db

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE Stepik_web_project;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON Stepik_web_project.* TO 'box'@'localhost' WITH GRANT OPTION;"
+python3 manage.py makemigrations qa
+python3 manage.py migrate
 sudo gunicorn -c hello.py hello:wsgi_app
 sudo gunicorn -c etc/ask_conf.py ask.wsgi:application
---
+++ update.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3.5
+sudo apt install python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.1
+sudo python3 -m pip install mysqlclient

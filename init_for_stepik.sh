6f3f787f5b995310ba797d32f45ec225ee1b2032
ldokovroman <8922684071@mail.ru>
2023-09-17 16:40:57 +0300

    hid sh-files

--- init_for_stepik.sh
+++
@@ -1,9 +0,0 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE Stepik_web_project;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON Stepik_web_project.* TO 'box'@'localhost' WITH GRANT OPTION;"
-python3 ask/manage.py makemigrations qa
-python3 ask/manage.py migrate qa
-sudo gunicorn -c etc/ask_conf.py ask.wsgi:application
